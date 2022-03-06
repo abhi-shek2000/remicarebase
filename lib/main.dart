f5ac1b695394f75d8949d9b6678d8e4446950825
Abhishek <abhi@Abhisheks-MacBook-Pro.local>
2022-03-06 16:24:26 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:mediminder/src/global_bloc.dart';
+import 'package:mediminder/src/ui/homepage/homepage.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MedicineReminder());
+}
+
+
+class MedicineReminder extends StatefulWidget {
+  @override
+  _MedicineReminderState createState() => _MedicineReminderState();
+}
+
+class _MedicineReminderState extends State<MedicineReminder> {
+  late GlobalBloc globalBloc;
+
+  void initState() {
+    globalBloc = GlobalBloc();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Provider<GlobalBloc>.value(
+      value: globalBloc,
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: Colors.green,
+          brightness: Brightness.light,
+        ),
+        home: HomePage(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
